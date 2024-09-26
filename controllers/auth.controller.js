781c39b1bff7f9d400c5bf541c62771bbe00774d
Ritu29verma <ritu1298verma@gmail.com>
2024-09-26 21:43:46 +0530

    ^

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,13 +8,24 @@ export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
+
   try {
     await newUser.save(); 
-    res.status(201).json({ message: 'User created successfully' });
+    
+    // Automatically log in after signup by generating a token
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = newUser._doc; 
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+
+    res
+      .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+      .status(201)
+      .json({ success: true,message: 'User registered successfully', user: rest });
   } catch (error) {
     next(error);
   }
 };
+
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
@@ -29,7 +40,7 @@ export const signin = async (req, res, next) => {
     res
       .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
       .status(200)
-      .json(rest);
+      .json({ success: true,message: 'Welcome back! You have successfully logged in.', user: rest });
   } catch (error) {
     next(error);
   }
